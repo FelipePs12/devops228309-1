3245e7872b0186be4d4863daf0934b3b6c161d26
FelipePs12 <felipe321ps@hotmail.com>
2023-11-24 22:44:43 -0300

    validacao contributions

--- app/data/contributions-dao.js
+++ app/data/contributions-dao.js
@@ -1,12 +1,9 @@
 const UserDAO = require("./user-dao").UserDAO;
 
-/* The ContributionsDAO must be constructed with a connected database object */
 function ContributionsDAO(db) {
     "use strict";
 
-    /* If this constructor is called without the "new" operator, "this" points
-     * to the global object. Log a warning and call it correctly. */
-    if (false === (this instanceof ContributionsDAO)) {
+    if (!(this instanceof ContributionsDAO)) {
         console.log("Warning: ContributionsDAO constructor called without 'new' operator");
         return new ContributionsDAO(db);
     }
@@ -17,7 +14,10 @@ function ContributionsDAO(db) {
     this.update = (userId, preTax, afterTax, roth, callback) => {
         const parsedUserId = parseInt(userId);
 
-        // Create contributions document
+        if (isNaN(parsedUserId)) {
+            return callback("Invalid userId", null);
+        }
+
         const contributions = {
             userId: parsedUserId,
             preTax: preTax,
@@ -25,62 +25,67 @@ function ContributionsDAO(db) {
             roth: roth
         };
 
-        contributionsDB.update({
-            userId
-            },
-            contributions, {
-                upsert: true
-            },
-            err => {
-                if (!err) {
-                    console.log("Updated contributions");
-                    // add user details
-                    userDAO.getUserById(parsedUserId, (err, user) => {
-
-                        if (err) return callback(err, null);
-
-                        contributions.userName = user.userName;
-                        contributions.firstName = user.firstName;
-                        contributions.lastName = user.lastName;
-                        contributions.userId = userId;
-
-                        return callback(null, contributions);
-                    });
-                } else {
+        contributionsDB.update(
+            { userId: parsedUserId },
+            contributions,
+            { upsert: true },
+            (err) => {
+                if (err) {
                     return callback(err, null);
                 }
+
+                console.log("Updated contributions");
+
+                userDAO.getUserById(parsedUserId, (err, user) => {
+                    if (err) {
+                        return callback(err, null);
+                    }
+
+                    contributions.userName = user.userName;
+                    contributions.firstName = user.firstName;
+                    contributions.lastName = user.lastName;
+
+                    return callback(null, contributions);
+                });
             }
         );
     };
 
     this.getByUserId = (userId, callback) => {
-        contributionsDB.findOne({
-                userId: userId
-            },
-            (err, contributions) => {
-                if (err) return callback(err, null);
+        const parsedUserId = parseInt(userId);
 
-                // Set defualt contributions if not set
+        if (isNaN(parsedUserId)) {
+            return callback("Invalid userId", null);
+        }
+
+        contributionsDB.findOne(
+            { userId: parsedUserId },
+            (err, contributions) => {
+                if (err) {
+                    return callback(err, null);
+                }
+
+                // Set default contributions if not set
                 contributions = contributions || {
                     preTax: 2,
                     afterTax: 2,
                     roth: 2
                 };
 
-                // add user details
-                userDAO.getUserById(userId, (err, user) => {
+                userDAO.getUserById(parsedUserId, (err, user) => {
+                    if (err) {
+                        return callback(err, null);
+                    }
 
-                    if (err) return callback(err, null);
                     contributions.userName = user.userName;
                     contributions.firstName = user.firstName;
                     contributions.lastName = user.lastName;
-                    contributions.userId = userId;
 
-                    callback(null, contributions);
+                    return callback(null, contributions);
                 });
             }
         );
     };
 }
 
-module.exports = { ContributionsDAO };
+module.exports = { ContributionsDAO };
