3991b270b092c74cbe49ced2621c87682bbc6322
FelipePs12 <felipe321ps@hotmail.com>
2023-11-24 22:37:28 -0300

    injection

--- app/data/allocations-dao.js
+++ app/data/allocations-dao.js
@@ -1,13 +1,9 @@
 const UserDAO = require("./user-dao").UserDAO;
 
-/* The AllocationsDAO must be constructed with a connected database object */
-const AllocationsDAO = function(db){
-
+const AllocationsDAO = function(db) {
     "use strict";
 
-    /* If this constructor is called without the "new" operator, "this" points
-     * to the global object. Log a warning and call it correctly. */
-    if (false === (this instanceof AllocationsDAO)) {
+    if (!(this instanceof AllocationsDAO)) {
         console.log("Warning: AllocationsDAO constructor called without 'new' operator");
         return new AllocationsDAO(db);
     }
@@ -18,97 +14,94 @@ const AllocationsDAO = function(db){
     this.update = (userId, stocks, funds, bonds, callback) => {
         const parsedUserId = parseInt(userId);
 
-        // Create allocations document
+        if (isNaN(parsedUserId)) {
+            return callback("Invalid userId", null);
+        }
+
         const allocations = {
-            userId: userId,
+            userId: parsedUserId,
             stocks: stocks,
             funds: funds,
             bonds: bonds
         };
 
-        allocationsCol.update({
-            userId: parsedUserId
-        }, allocations, {
-            upsert: true
-        }, err => {
+        allocationsCol.update(
+            { userId: parsedUserId },
+            allocations,
+            { upsert: true },
+            (err) => {
+                if (!err) {
+                    console.log("Updated allocations");
 
-            if (!err) {
+                    userDAO.getUserById(userId, (err, user) => {
+                        if (err) {
+                            return callback(err, null);
+                        }
 
-                console.log("Updated allocations");
+                        allocations.userId = userId;
+                        allocations.userName = user.userName;
+                        allocations.firstName = user.firstName;
+                        allocations.lastName = user.lastName;
 
-                userDAO.getUserById(userId, (err, user) => {
-
-                    if (err) return callback(err, null);
-
-                    // add user details
-                    allocations.userId = userId;
-                    allocations.userName = user.userName;
-                    allocations.firstName = user.firstName;
-                    allocations.lastName = user.lastName;
-
-                    return callback(null, allocations);
-                });
+                        callback(null, allocations);
+                    });
+                } else {
+                    callback(err, null);
+                }
             }
-
-            return callback(err, null);
-        });
+        );
     };
 
     this.getByUserIdAndThreshold = (userId, threshold, callback) => {
         const parsedUserId = parseInt(userId);
 
-        const searchCriteria = () => {
+        if (isNaN(parsedUserId)) {
+            return callback("Invalid userId", null);
+        }
 
+        const searchCriteria = () => {
             if (threshold) {
-                /*
-                // Fix for A1 - 2 NoSQL Injection - escape the threshold parameter properly
-                // Fix this NoSQL Injection which doesn't sanitze the input parameter 'threshold' and allows attackers
-                // to inject arbitrary javascript code into the NoSQL query:
-                // 1. 0';while(true){}'
-                // 2. 1'; return 1 == '1
-                // Also implement fix in allocations.html for UX.                             
-                const parsedThreshold = parseInt(threshold, 10);
-                
-                if (parsedThreshold >= 0 && parsedThreshold <= 99) {
-                    return {$where: `this.userId == ${parsedUserId} && this.stocks > ${parsedThreshold}`};
+                const parsedThreshold = parseFloat(threshold);
+                if (!isNaN(parsedThreshold) && parsedThreshold >= 0 && parsedThreshold <= 99) {
+                    return {
+                        userId: parsedUserId,
+                        stocks: { $gt: parsedThreshold }
+                    };
                 }
-                throw `The user supplied threshold: ${parsedThreshold} was not valid.`;
-                */
-                return {
-                    $where: `this.userId == ${parsedUserId} && this.stocks > '${threshold}'`
-                };
             }
-            return {
-                userId: parsedUserId
-            };
+            return { userId: parsedUserId };
         };
 
         allocationsCol.find(searchCriteria()).toArray((err, allocations) => {
-            if (err) return callback(err, null);
-            if (!allocations.length) return callback("ERROR: No allocations found for the user", null);
+            if (err) {
+                return callback(err, null);
+            }
+            if (!allocations.length) {
+                return callback("ERROR: No allocations found for the user", null);
+            }
 
-            let doneCounter = 0;
-            const userAllocations = [];
-
-            allocations.forEach( alloc => {
-                userDAO.getUserById(alloc.userId, (err, user) => {
-                    if (err) return callback(err, null);
-
-                    alloc.userName = user.userName;
-                    alloc.firstName = user.firstName;
-                    alloc.lastName = user.lastName;
-
-                    doneCounter += 1;
-                    userAllocations.push(alloc);
-
-                    if (doneCounter === allocations.length) {
-                        callback(null, userAllocations);
-                    }
-                });
+            Promise.all(allocations.map(alloc =>
+                new Promise((resolve, reject) => {
+                    userDAO.getUserById(alloc.userId, (err, user) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            alloc.userName = user.userName;
+                            alloc.firstName = user.firstName;
+                            alloc.lastName = user.lastName;
+                            resolve(alloc);
+                        }
+                    });
+                })
+            ))
+            .then(userAllocations => {
+                callback(null, userAllocations);
+            })
+            .catch(err => {
+                callback(err, null);
             });
         });
     };
-
 };
 
 module.exports.AllocationsDAO = AllocationsDAO;
